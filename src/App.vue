78d6d6e0d1076a236bcff48a2a5e0df73200b144
Hasan Parasteh <hasanparasteh@gmail.com>
2022-02-02 20:23:50 +0330

    feat: make it work with vue3

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,109 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div class="wrapper" v-if="sliderValues && sliderValues.length">
+      <custom-slider
+        min="10"
+        max="50"
+        step="1"
+        :trackBgColor="'black'"
+        :trackColor="'red'"
+        v-model="slider"
+        :values="sliderValues2"
+      />
+    </div>
+    {{ slider }}
+
+    <button @click="changeValue()">Change value to 2nd option</button>
+
+    <button @click="changeOptions()">Change options</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { defineComponent } from "vue";
+import CustomSlider from "./components/CustomRangeSlider.vue";
 
-export default {
-  name: 'App',
+export default defineComponent({
+  name: "app",
   components: {
-    HelloWorld
-  }
-}
+    CustomSlider,
+  },
+  data() {
+    return {
+      changed: false,
+      slider: "20",
+      sliderValues: null,
+      sliderValues1: [
+        {
+          label: "Not at all",
+          value: "a",
+        },
+        {
+          label: "A tiny bit",
+          value: "b",
+        },
+        {
+          label: "Its ok",
+          value: "c",
+        },
+        {
+          label: "Its very good",
+          value: "d",
+        },
+        {
+          label: "Its AMAZING!",
+          value: "e",
+        },
+      ],
+      sliderValues2: [
+        {
+          label: "Something else",
+          value: "1",
+        },
+        {
+          label: "Something else else",
+          value: "2",
+        },
+        {
+          label: "Comp",
+          value: "3",
+        },
+      ],
+    };
+  },
+  mounted() {
+    this.sliderValues = this.sliderValues1;
+  },
+  methods: {
+    changeOptions() {
+      this.sliderValues = this.changed
+        ? this.sliderValues1
+        : this.sliderValues2;
+      this.changed = !this.changed;
+    },
+    changeValue() {
+      this.slider = this.sliderValues[1].value;
+    },
+  },
+});
 </script>
 
 <style>
+body {
+  margin: 0;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  background: #fff;
+  padding: 0 50px;
+  height: 100vh;
+  margin: 0;
+}
+
+.wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
